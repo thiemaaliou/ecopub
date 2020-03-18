28d51aca2b6e04293f746e7bc75747396d9d9982
Aliou Thiema <aliouthiema@gmail.com>
2020-03-18 21:12:27 +0000

    remove menu

--- src/app/shared/menus/organization-menu.ts
+++ src/app/shared/menus/organization-menu.ts
@@ -5,11 +5,6 @@ export const OrganizationMenus = [
     icon: 'map'
   },
   {
-    title: 'Publicités',
-    url: '/publicities',
-    icon: 'trash'
-  },
-  {
     title: 'Gestion Utilisateurs',
     url: '/manager-users',
     icon: 'build'
